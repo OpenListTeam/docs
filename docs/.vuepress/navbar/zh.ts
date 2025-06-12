759a98a5ef199d6cc6f3cbbe8a44d9b49cdb322e
Hantong Chen <cxwdyx620@gmail.com>
2025-06-12 13:17:27 +0800

    chore: update project name (partial: index page)

--- docs/.vuepress/navbar/zh.ts
+++ docs/.vuepress/navbar/zh.ts
@@ -5,32 +5,32 @@ export const zh = navbar([
   { text: "使用指南", icon: "iconfont icon-creative", link: "/zh/guide/" },
   { text: "配置", icon: "iconfont icon-config", link: "/zh/config/" },
   { text: "常见问题", icon: "iconfont icon-question", link: "/zh/faq/" },
-  { text: "定价", icon: "iconfont icon-tag", link: "/zh/pricing" },
+  // { text: "定价", icon: "iconfont icon-tag", link: "/zh/pricing" },
   // { text: "工具", "iconfont icon-tool", link: "/tool/" },
   {
     text: "社区",
     icon: "iconfont icon-community",
     children: [
-      // {
-      //   text: "Discussion",
-      //   link: "https://github.com/alist-org/alist/discussions",
-      // },
-      // { text: "Telegram", link: "https://t.me/alist_chat" },
+      {
+        text: "Discussion",
+        link: "https://github.com/OpenListTeam/OpenList/discussions",
+      },
+      { text: "Telegram", link: "https://t.me/+BYuDkV2f-ds2ODE1" },
       // { text: "Discord", link: "https://discord.gg/F4ymsH4xv2" },
     ],
   },
-  {
-    text: "更多",
-    icon: "iconfont icon-more",
-    children: [
-      { text: "演示", link: "https://alist.pages.dev/" },
-      { text: "V2", link: "https://alist-doc.example.com" },
-      { text: "桌面版", link: "https://ad.example.com/" },
-    ],
-  },
+  // {
+  //   text: "更多",
+  //   icon: "iconfont icon-more",
+  //   children: [
+  //     { text: "演示", link: "https://alist.pages.dev/" },
+  //     { text: "V2", link: "https://alist-doc.example.com" },
+  //     { text: "桌面版", link: "https://ad.example.com/" },
+  //   ],
+  // },
   {
     text: "Github",
     icon: "iconfont icon-github",
-    link: "https://github.com/AlistTeam/alist",
+    link: "https://github.com/OpenListTeam/OpenList",
   },
 ])
